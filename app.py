39ff6ebf207914d7c295a1c58eff7e6307c99231
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2023-10-16 15:47:37 +0300

    Implement jinja2 blocks in base and html files.
    Convert .html files to jinja2.

--- app.py
+++ app.py
@@ -12,9 +12,11 @@ posts = {
 
 @app.route('/post/<int:post_id>') #/post/0
 def post_id(post_id):
-    #post = posts.get(post_id)
+    post = posts.get(post_id)
+    if not post: #If posts.get(post_id) does not have the key, it will return None, and None is False.
+        return render_template("404.jinja2", message = f"A post with id {post_id} is not found.")
     #return (f'Post {post["title"]}, content: \n\n {post["content"]}')
-    return render_template('post.html', post = posts.get(post_id))
+    return render_template('post.jinja2', post = post)
 
 @app.route('/')
 def home():
