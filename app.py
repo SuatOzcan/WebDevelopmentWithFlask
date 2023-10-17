b2e38509eed6a0b895b86d8f2fad97ad661eb2a4
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2023-10-17 14:23:49 +0300

    Create /post/form.

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 #HTML files should be in 'templates' folder.
 
 app = Flask(__name__)
 
 posts = {
     0 : {
+        'id' : 0,
         'title' : 'Hello World!',
         'content' : 'This is my first blog post.'
     }
 }
 
+@app.route('/')
+def home():
+    return ('Hello World!')
+
 @app.route('/post/<int:post_id>') #/post/0
-def post_id(post_id):
+def post(post_id):
     post = posts.get(post_id)
     if not post: #If posts.get(post_id) does not have the key, it will return None, and None is False.
         return render_template("404.jinja2", message = f"A post with id {post_id} is not found.")
     #return (f'Post {post["title"]}, content: \n\n {post["content"]}')
     return render_template('post.jinja2', post = post)
 
-@app.route('/')
-def home():
-    return ('Hello World!')
+@app.route('/post/form')
+def form():
+    print(posts)
+    return render_template('create.jinja2')
+
+#127.0.0.1:5000/post/create?title=placeholder&content=anotherplaceholder
+@app.route('/post/create')
+def create():
+    title = request.args.get('title')
+    content = request.args.get('content')
+    post_id = len(posts)
+    posts[post_id] = {'id' : post_id, 'title' : title, 'content' : content}
+    return redirect(url_for('post', post_id = post_id))
 
 if __name__ == '__main__':
     app.run(debug = True)
